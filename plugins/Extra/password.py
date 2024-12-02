cbb18e7688a4cc0334051f2fe93cfc352b5a20e7
MN BOTS <mohammedmusammiln@gmail.com>
2024-12-02 21:41:20 +0530

    Update password.py

--- plugins/Extra/password.py
+++ plugins/Extra/password.py
@@ -17,5 +17,6 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('MN Bots', url='https://t.me/mnbots')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('MN Bots', url='https://t.me/mnbots'),
+                                 InlineKeyboardButton('ʀᴇᴘᴏ', url='https://github.com/mn-bots/ShobanaFilterBot')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
