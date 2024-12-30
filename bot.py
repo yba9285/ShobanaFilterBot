e0834199e6a7aef84c4569d2eed22c0a1b4bd53a
MN - TG <mntg3509@gmail.com>
2024-12-30 20:08:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,7 +51,7 @@ class Bot(Client):
             sleep_threshold=5,
         )
 
-    async def start(self):
+    async def start(self,**kwargs):
         b_users, b_chats = await db.get_banned()
         temp.BANNED_USERS = b_users
         temp.BANNED_CHATS = b_chats
