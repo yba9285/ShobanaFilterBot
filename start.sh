b4b56daa63ca07fd71f8eaed3b391f77878869e7
MN - TG <mntg3509@gmail.com>
2025-01-01 21:45:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/GouthamSER/KuttuBot.git /KuttuBot
+  git clone https://github.com/mn-bots/shobanafilterbot.git /KuttuBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /KuttuBot
