880883da6dec65e4beecf6d969d87362d6dbf312
MN-TG <mohammedmusammiln@gmail.com>
2025-05-29 12:08:30 +0530

    Create mntgxo.py

---
+++ plugins/mntgxo.py
@@ -0,0 +1,9 @@
+#  @MrMNTG @MusammilN 
+from pyrogram import filters, Client
+from pyrogram.types import Message
+from utils import JOIN_REQUEST_USERS
+
+@Client.on_message(filters.command("clear_join_users") & filters.user(1892771262))
+async def clear_join_users(_, message: Message):
+    JOIN_REQUEST_USERS.clear()
+    await message.reply_text("✅ Cleared all join request users.")
