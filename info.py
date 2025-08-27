8129725069e57d9316c5f71cba3527d1e1dde907
yba9285 <108516941+yba9285@users.noreply.github.com>
2025-08-27 12:44:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,7 +33,7 @@ PICS = (environ.get('PICS', 'https://envs.sh/tzB.jpg https://envs.sh/tzI.jpg htt
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1731356432').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002124208809').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002711685776').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_grp = environ.get('AUTH_GROUP')
